34a5f96d119853689c3ab2c60fad3acf7ca23c2c
Gabriel-U-Sa <gabriel291108@gmail.com>
2025-08-22 23:20:11 -0500

    modifica el app

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = ["Luis"];
-
+let amigos = [" "];
+//Función para agregar texto a un elemento HTML
 function agregarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
+    //elementoHTML.innerHTML = texto;
     return;
 }
 
+//Función para agregar amigo a la lista
 function agregarAmigo() {
     let nombreAmigo = (document.getElementById('amigo').value).trim();
-    if (nombreAmigo === '') {
-        alert ('El nombre del amigo no puede estar vacío.');
-    }else{
-        amigos.push(nombreAmigo);
-        agregarTextoElemento('p',`Amigo agregado: ${nombreAmigo}`);
-        document.getElementById('agregar').removeAttribute('disabled','true');
-        limpiarCaja();
-        mostrarAmigos();
-    }
-    //document.getElementById('agregar').removeAttribute('disabled');
-    return;
+    for (let i = 0; i < amigos.length; i++) {    
+        if (nombreAmigo === '') {
+        //Envio alerta si el campo esta vacio
+            alert ('El nombre del amigo no puede estar vacío.');
+        }else{
+            //Agregar amigo a la lista
+            amigos.push(nombreAmigo);
+            document.getElementById('agregar').removeAttribute('disabled');
+            limpiarCaja();
+            //mostrarAmigos();
+            console.log(amigos);
+        }
+        return;
+    } 
 }
 agregarAmigo();
+
+//Función para limpiar la caja de texto
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
-console.log(amigos);
+
+agregarTextoElemento('h1', 'Lista de amigos');
