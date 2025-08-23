c3523c18f9cd53d77d47249d469b32eabfaecc7f
Gabriel-U-Sa <gabriel291108@gmail.com>
2025-08-22 19:32:03 -0500

    actualiza el app y el index

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
+let amigos = ["Luis"];
+
+function agregarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo() {
+    let nombreAmigo = (document.getElementById('amigo').value).trim();
+    if (nombreAmigo === '') {
+        alert ('El nombre del amigo no puede estar vacío.');
+    }else{
+        amigos.push(nombreAmigo);
+        agregarTextoElemento('p',`Amigo agregado: ${nombreAmigo}`);
+        document.getElementById('agregar').removeAttribute('disabled','true');
+        limpiarCaja();
+        mostrarAmigos();
+    }
+    //document.getElementById('agregar').removeAttribute('disabled');
+    return;
+}
+agregarAmigo();
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+console.log(amigos);
