a9e69548d48f618a663919d01bfd7477bb3fa4b7
Gabriel-U-Sa <gabriel291108@gmail.com>
2025-08-24 15:56:33 -0500

    actualiza el app

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [" "];
-//Función para agregar texto a un elemento HTML
-function agregarTextoElemento(elemento, texto){
+let amigos =[];
+//Función para asignar texto a un elemento HTML
+function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
-    //elementoHTML.innerHTML = texto;
+    elementoHTML.innerHTML = texto;
     return;
 }
-
 //Función para agregar amigo a la lista
-function agregarAmigo() {
-    let nombreAmigo = (document.getElementById('amigo').value).trim();
-    for (let i = 0; i < amigos.length; i++) {    
-        if (nombreAmigo === '') {
-        //Envio alerta si el campo esta vacio
-            alert ('El nombre del amigo no puede estar vacío.');
-        }else{
-            //Agregar amigo a la lista
-            amigos.push(nombreAmigo);
-            document.getElementById('agregar').removeAttribute('disabled');
-            limpiarCaja();
-            //mostrarAmigos();
-            console.log(amigos);
-        }
-        return;
-    } 
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value.trim();
+    if (nombreAmigo === '') {
+        //Envío alerta si el campo está vacío
+        alert('El nombre del amigo no puede estar vacío.');
+    }else {
+        //Agregar amigo al array
+        amigos.push(nombreAmigo);
+        document.getElementById("amigo").value = '';
+        limpiarCaja();
+        //console.log(amigos);
+        mostrarAmigos();    
+    }
+    return;
 }
-agregarAmigo();
 
 //Función para limpiar la caja de texto
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
+    
 }
-
-agregarTextoElemento('h1', 'Lista de amigos');
+//Función para mostrar amigos en la lista
+function mostrarAmigos(){
+    let listaAmigos = '';
+    amigos.forEach(amigo => {
+        listaAmigos += `<li>${amigo}</li>`;
+    });
+    asignarTextoElemento('ul', listaAmigos);
+}
